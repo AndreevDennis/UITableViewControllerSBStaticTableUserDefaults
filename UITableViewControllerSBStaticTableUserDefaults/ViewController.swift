d9ccc66308e3ff9382c3bed42840d3c48a6e620c
Dennis <denisandreev1611@gmail.com>
2019-05-31 19:50:16 +0300

    Initial Commit

---
+++ UITableViewControllerSBStaticTableUserDefaults/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITableViewControllerSBStaticTableUserDefaults
+//
+//  Created by Denis Andreev on 5/31/19.
+//  Copyright © 2019 353. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
